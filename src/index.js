06d93620fabf46fa398a7644ce40a6b41bace872
IshigureTomoki <71107821+IshigureTomoki@users.noreply.github.com>
2021-11-17 00:39:09 +0900

    初期値削除

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  if (inputText !== "") {
+    createIncompleteList(inputText);
+  }
+};
 
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("im").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteList = (text) => {
   const li = document.createElement("li");
   const div = document.createElement("div");
   div.className = "list-row";
 
   const p = document.createElement("p");
   p.className = "list";
-  p.innerText = inputText;
+  p.innerText = text;
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
@@ -31,6 +42,14 @@ const onClickAdd = () => {
 
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("comp").removeChild(deleteTarget);
+
+      const text = backButton.parentNode.firstChild.innerText;
+
+      createIncompleteList(text);
+    });
 
     addTarget.firstChild.appendChild(p);
     addTarget.firstChild.appendChild(backButton);
@@ -50,11 +69,6 @@ const onClickAdd = () => {
   li.appendChild(div);
 
   document.getElementById("im").appendChild(li);
-};
-
-//未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("im").removeChild(target);
 };
 
 document
